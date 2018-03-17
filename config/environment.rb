64aade8796430482c0a5e282bfa9292d4eb9d60a
Doris <DorisMac@doriss-mbp.speedport_w_723v_1_45_000>
2018-03-17 14:25:13 +0100

    ENV

--- config/environment.rb
+++ config/environment.rb
@@ -8,12 +8,12 @@ require_relative 'application'
 Rails.application.initialize!
 
 
-ActionMailer::Base.smtp_settings = {
-  :port           => ENV['25'],
-  :address        => ENV['smtp.mailgun.org'],
-  :user_name      => ENV['postmaster@sandbox4da2a91014c44e5d98bff970fa7a4f2f.mailgun.org'],
-  :password       => ENV['e3ea952f3348d5ca5159f25469e3e895-833f99c3-ad7431dd'],
-  :domain         => 'glacial-lake-54827.herokuapp.com',
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
   :authentication => :plain,
+  :address => "smtp.mailgun.org",
+  :port => 587,
+  :domain => "glacial-lake-54827.herokuapp.com",
+  :user_name => "postmaster@sandbox4da2a91014c44e5d98bff970fa7a4f2f.mailgun.org",
+  :password => "e3ea952f3348d5ca5159f25469e3e895-833f99c3-ad7431dd"
 }
-ActionMailer::Base.delivery_method = :smtp
