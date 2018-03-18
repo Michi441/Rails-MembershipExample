99b7be40413861c6e7a6978e879139af50aac3aa
Doris <DorisMac@doriss-mbp.speedport_w_723v_1_45_000>
2018-03-18 18:39:54 +0100

    Working on stripe js logic

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,26 +1,78 @@
-$(document).on('turbolinks:load', function(){
 
+// LOAD THE DOCUMENT VIA JQUERY & TURBOLINKS
+$(document).on('turbolinks:load', function(){
+  // GET THE PRO_FORM FROM _PRO_FORM.HTML.ERB
   var Form = $('#pro_form');
+
+  // GET THE SUBMIT BUTTON IN THE _PRO_FORM.HTMl.ERB
   var SubmitBtn = $('#form-submit-btn');
+
+  // SETUP THE STRIPE API KEY
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
 
-
+  // IF THE BUTTON GETS CLICKED
   SubmitBtn.click(function(event){
+
+    // MAKE SURE ITS NOT EMPTY
     event.preventDefault();
 
+    submitBtn.val('Processing').prop('disabled', true);
 
+    // ASSIGN VARIABLES TO THE DATA INSIDE THE FIELDS
     var ccNum = $('#card_number').val(),
-      cvcNum = $('#card_code').val(),
-      expMonth = $('#card_month').val(),
-      expYear = $('#card_year').val();
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
+
+    //USE STRIPE LIBRARY FOR ERRORS
+
+    var error = false
+
+    if(!Stripe.card.validateCardNumber(ccNum)){
+
+      error = true;
+      alert('The credit card seems to be invalid!');
+    };
+
+    if(!Stripe.card.validateCVC(cvcNum)){
+
+      error = true;
+      alert('The CVC seems to be invalid!');
+    };
+
+    if(!Stripe.card.validateExpiry(expMonth, expYear)){
+
+      error = true;
+      alert('The expiration date appears to be invalid!');
+    };
 
 
-    Stripe.createToken({
-      number: ccNum,
-      cvc: cvcNum,
-      exp_month: expMonth,
-      exp_year: expYear
-    }, stripeResponseHandler)
+    if(error){
+
+      submitBtn.prop('disabled', false).val('Sign Up');
+      // if there are errors, don't send it to stripe
+    } else {
+      // IF NOT, CREATE A TOKEN BASED ON THE VALUES OF THE VARIABLES.
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler)
+
+    }
+
+
+
+
   });
 
+
+function stripeResponseHandler(status, response){
+  var token = response.id;
+
+  Form.append($('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+
+  Form.get(0).submit();
+}
 });
