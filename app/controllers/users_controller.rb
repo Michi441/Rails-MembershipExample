edef3b2832a1c3fd5b02e5da5f92f91d705706b7
Doris <DorisMac@Doriss-MBP.local>
2018-03-23 15:49:02 +0100

    Added index for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
 
+
+  def index
+    @users = User.all
+  end
+  
   def show
     @user = User.find(params[:id])
+
+    @subscription = Stripe::Customer.retrieve(@user.stripe_costumer_token)
   end
 
 end
